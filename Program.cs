75752ccdac776c58b9529ece1761b8f1a4191f87
Aidana <aidanaleupen@gmai.com>
2023-07-20 20:43:22 +0600

    001

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// программа принимает координаты точки (x, y) и выдает 
+// номер четверти плоскости. X и Y не равны нулю. 
+
+Console.Write("x = ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("y = ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0)
+    Console.Write("1");
+else if (x < 0 && y > 0)
+    Console.Write("2");
+else if (x < 0 && y < 0)
+    Console.Write("3");
+else if (x > 0 && y < 0)
+    Console.Write("4");
